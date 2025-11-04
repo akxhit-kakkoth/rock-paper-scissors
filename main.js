96764de4537758e87ad5c9718ffb7b21d198b438
AKSHIT <akshitkakkoth802@gmail.com>
2025-11-04 08:41:08 +0530

    count added

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ let userChoicePara = document.getElementById("userChoice-para");
 let computerChoicePara = document.getElementById("computerChoice-para");
 let resultPara = document.getElementById("result-para");
 
+let userScoreEl=document.getElementById('user-score');
+let computerScoreEl=document.getElementById('computer-score');
+let drawScoreEl=document.getElementById('draw-score');
+
+let userScore=0;
+let computerScore=0;
+let drawScore=0;
 function playGame(userChoice) {
   console.log(userChoice);
 
@@ -15,13 +22,20 @@ function playGame(userChoice) {
 
   if (computerChoice === userChoice) {
     resultPara.innerText = "Result: It's Draw";
+    drawScore=drawScore+1;
+    drawScoreEl.innerText=drawScore;
+
   } else if (
     (userChoice === "rock" && computerChoice === "scissors") ||
     (userChoice === "paper" && computerChoice === "rock") ||
     (userChoice === "scissors" && computerChoice === "paper")
   ) {
     resultPara.innerText = "Result: You Won!!";
+    userScore=userScore+1;
+    userScoreEl.innerText=userScore;
   } else {
     resultPara.innerText = "Result: You Lost";
+    computerScore=computerScore+1;
+    computerScoreEl.innerText=computerScore;
   }
 }
